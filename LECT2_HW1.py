a9036ec2ba15c6899b219db13bd075b53e47fa69
Yulia Magonya <ymagonia@gmail.com>
2024-04-15 23:34:19 +0300

    Update LECT2_HW1.py
    re-checked the issue, found my mistake regarding sep=

--- LECT2_HW1.py
+++ LECT2_HW1.py
@@ -1,5 +1,6 @@
 list_a = ['Murzik', 'Barsik', 'Pantera']
 
-for x in range(len(list_a)):
-    print(list_a[x], sep=', ', end=' ')
+#for x in range(len(list_a)):
+#    print(list_a[x], sep=', ', end=' ')
 
+print(f"{', '.join([str(i) for i in list_a])}")
